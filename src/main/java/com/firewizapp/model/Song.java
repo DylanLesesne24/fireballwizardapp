c5e811be0be11dbf6fa8e1d3d2d20c0b61a5d415
DylanLesesne24 <dlesesne@email.sc.edu>
2025-03-06 14:17:51 -0500

    added song.java + userlist.java

--- src/main/java/com/firewizapp/model/Song.java
+++ src/main/java/com/firewizapp/model/Song.java
@@ -1,5 +1,28 @@
 package com.firewizapp.model;
 
+import java.util.HashMap;
+
 public class Song {
 
+    public static HashMap<Integer, Song> SONGS = new HashMap<>();
+
+    private String title;
+    private String artist;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public static Song getSong(int num) {
+        return SONGS.get(num); 
+    }
 }
