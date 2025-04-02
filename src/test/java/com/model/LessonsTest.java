5f8d6a341c6e38bb5e71507f1c06cacd8bb748a0
Landen Worthy <lworthy@email.sc.edu>
2025-04-02 11:50:59 -0400

    Correct imports

--- src/test/java/com/model/LessonsTest.java
+++ src/test/java/com/model/LessonsTest.java
@@ -1,5 +1,15 @@
 package com.model;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.firewizapp.model.Lessons;
+import com.firewizapp.model.Difficulty;
+import com.firewizapp.model.Flashcards;
+import com.firewizapp.model.Quiz;
+
 public class LessonsTest {
     
 }
